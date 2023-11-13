3c74c2a3aed9eb096112583b0cce3d1b0eefce7d
pipe <felipe.parra@blackmind.cl>
2023-11-13 01:46:19 -0300

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FabricComponent } from './fabric/fabric.component';
+import { ProductsComponent } from './products/products.component';
+import { PromoComponent } from './promo/promo.component';
+import { PromiseComponent } from './promise/promise.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: HomeComponent},
+  { path: 'navbar', component: NavbarComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'fabric', component: FabricComponent},
+  { path: 'products', component: ProductsComponent},
+  { path: 'promo', component: PromoComponent},
+  { path: 'promise', component: PromiseComponent},
+  { path: 'cart', component: CartComponent},         
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { FabricComponent } from './fabric/fabric.component';
+import { PromiseComponent } from './promise/promise.component';
+import { CartComponent } from './cart/cart.component';
+import { PromoComponent } from './promo/promo.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductsComponent,
+    FabricComponent,
+    PromiseComponent,
+    CartComponent,
+    PromoComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
